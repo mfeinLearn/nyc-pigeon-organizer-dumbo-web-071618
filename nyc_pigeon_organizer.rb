e50b89935e7ac223474ec32d2b9a1e877c420884
Malcome Fein <mfein90@gmail.com>
2018-08-04 05:29:12 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -52,4 +52,14 @@ require 'pry'
 #     pigeon_list
 # end
 
-
+def nyc_pigeon_organizer(data)
+  data.each_with_object({}) do |(key, h), result|
+    h.each do |value, names|
+      names.each do |name|
+        result[name] ||= {}
+        result[name][key] ||= []
+        result[name][key] << value.to_s
+      end
+    end
+  end
+end
